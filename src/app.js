c48ca2cca3d34ba414b4764edcdcf4ea00a0f2a1
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-04-30 19:49:57 +0800

    Implement users CRUD

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-import authRoutes from '#routes/auth.routes.js';
 import logger from '#config/logger.js';
+import authRoutes from '#routes/auth.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
 
@@ -29,10 +30,15 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
 });
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Docker Acquisitions API is running..'});
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
