60a811414eb4e98986ca8e6fb4d0db73ac0ed3b5
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-04-30 14:10:08 +0800

    implementing folders

--- src/server.js
+++ src/server.js
@@ -1,1 +1,7 @@
-import express from 'express';
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
